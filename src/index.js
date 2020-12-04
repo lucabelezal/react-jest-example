bc78bede3697244a8d0821b1a809a9150f810244
Lucas Nascimento <leddnascimento@gmail.com>
2020-12-03 22:02:08 -0300

    Adiciona projeto

---
+++ src/index.js
@@ -0,0 +1,4 @@
+exports.fn = arg => {
+    if (arg < 0) return 0;
+    return arg + 1;
+};
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+const {fn} = require('.');
+
+describe('fn', () => {
+    test('adds 1', () => {
+        expect(2).toEqual(2);
+    });
+});
